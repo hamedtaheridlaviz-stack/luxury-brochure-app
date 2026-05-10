c681f38e98c6e52e83d1aecbca0bd8d03e5a5e8e
hamedtaheridlaviz-stack <hamedtaheridlaviz@gmail.com>
2026-05-10 15:31:28 +0400

    Delete mock-import.js

--- mock-import.js
+++
@@ -1,4 +0,0 @@
-import { mockListing } from "../lib/listingParser.js";
-export default async function handler(req, res) {
-  return res.status(200).json({ ...mockListing(), version: "3.0.0-final-import-system", importMethod: "mock-test" });
-}
